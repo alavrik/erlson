e562bb05b28afd5154d044d858e1659cf052daba
Anton Lavrik <alavrik@piqi.org>
2013-02-06 02:01:23 -0600

    Add ?erlson_default/1 and ?erlson_default/2 macros
    ?erlson_default(X.foo, Default) will return the provided Default value if field
    "foo" is missing in the record.
    
    ?erlson_default(X.foo) is the same as ?erlson_default(X.foo, 'undefined').

--- include/erlson.hrl
+++ include/erlson.hrl
@@ -3,4 +3,16 @@
 
 -compile({parse_transform, erlson_pt}).
 
+
+-define(erlson_default(X, Default),
+    try
+        X
+    catch error:'erlson_not_found' ->
+        Default
+    end
+).
+
+-define(erlson_default(X), ?erlson_default(X, 'undefined')).
+
+
 -endif.
