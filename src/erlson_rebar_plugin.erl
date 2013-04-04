e817a39f56d887deeeb935d666959a9d3e9a7d5b
Nicholas Lundgaard <nlundgaard@alertlogic.com>
2013-04-04 13:37:50 -0500

    Changing rebar plugin to use shell init instead of erlc init in pre_doc/2

--- src/erlson_rebar_plugin.erl
+++ src/erlson_rebar_plugin.erl
@@ -19,15 +19,24 @@ pre_eunit(Config, X) ->
 pre_ct(Config, X) ->
     common(Config, X).
 
-pre_doc(Config, X) ->
-    common(Config, X).
-
 pre_xref(Config, X) ->
     common(Config, X).
 
+pre_doc(Config, X) ->
+    % we have to load 'erl_parse_shell' (by calling erlson:init()) instead of
+    % 'erl_parse_erlc`, because, unlike erlc (used by "rebar compile"), edoc
+    % (used by "rebar doc") doesn't apply parse transformations and therefore
+    % can't recognize Erlson syntax elements
+    erlson:init(),
+    common_1(Config, X).
 
-common(_Config, _X) ->
+
+common(Config, X) ->
     erlson:init_erlc(),
+    common_1(Config, X).
+
+
+common_1(_Config, _X) ->
     rebar_log:log(debug,
         "erlson_rebar_plugin: loaded erl_parse as ~s~n",
         [code:which(erl_parse)]),
